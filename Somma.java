f44209768e12c3b5bc2473bbf9feb3b6cacca190
Kowalski Prime <kowalski.7cc@xspacesoft.com>
2018-01-21 21:06:28 +0100

    Update Java: Allow numbers of unlimited size using BigDigits class!

--- Somma.java
+++ Somma.java
@@ -1,6 +1,4 @@
-/**
- * Somma
- */
+import java.math.BigDecimal;
 public class Somma {
 
   public static void main(String[] args) {
@@ -8,22 +6,25 @@ public class Somma {
       System.out.println("Argomenti non sufficienti");
     } else {
       try {
-        System.out.println(somma(args[0]));
+        System.out.println("Result: " + somma(args[0]));
       } catch (NumberFormatException e) {
-        System.out.println("Errore nella computazione dell'input: " + e);
+        System.out.println("Error during elaboration: " + ((e.getMessage() ==null)
+          ? "Invalid character in list"
+          : e.getMessage()));
       }
       
     }
   }
   
-  public static float somma(String myString) throws NumberFormatException {
-    String[] numbers = myString.split(" ");
-    float sum = 0;
-    for (String current : numbers) {
-      if (current.length() != 0)
-        sum += Float.parseFloat(current);
-    }
-    return sum;
-  }
+  public static BigDecimal somma(String myString) throws NumberFormatException {
+		BigDecimal bigDecimal = new BigDecimal(0);
+	    String[] numbers = myString.split(" ");
+	    for (String current : numbers) {
+	      if (current.length() != 0) {
+	    	  bigDecimal = bigDecimal.add(new BigDecimal(current));
+	      }
+	    }
+	    return bigDecimal;
+	  }
   
 }
